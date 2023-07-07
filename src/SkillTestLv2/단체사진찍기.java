64a4bc21cc5830904dba0f66e96763ebf6dfc84b
zack8361 <zack6353@naver.com>
2023-07-07 20:52:52 +0900

    벼ㄱ느낌
    벼ㄱ느낌

---
+++ src/SkillTestLv2/단체사진찍기.java
@@ -0,0 +1,15 @@
+package SkillTestLv2;
+
+public class 단체사진찍기 {
+    public static void main(String[] args) {
+        int n = 2;
+        String[] data = {"N~F=0", "R~T>2"};
+        System.out.println(solution(n,data));
+    }
+
+    private static int solution(int n, String[] data) {
+        
+
+        return 0;
+    }
+}
---
+++ src/SkillTestLv2/콜라츠추측.java
@@ -0,0 +1,14 @@
+package SkillTestLv2;
+
+public class 콜라츠추측 {
+    public static void main(String[] args) {
+        int k = 5;
+        int[][] ranges = {{0,0},{0,-1},{2,-3},{3,-3}};
+        System.out.println(solution(k,ranges));
+    }
+
+    private static double[] solution(int k, int[][] ranges) {
+
+        return new double[0];
+    }
+}
