f296781f3e895ff2fac0d04dae1a9c00563d58f7
zack8361 <zack6353@naver.com>
2023-07-20 01:29:37 +0900

    투포인터 넘어렵네
    투포인터 넘어렵네

--- src/danawa/ex05.java
+++ src/danawa/ex05.java
@@ -78,7 +78,6 @@ public class ex05 {
                     }
                 }
             }
-
 //            size
             else {
                 System.out.println(pq.size());
--- src/프로그래머스2단계100점못맞은문제들/연속된부분수열의합2.java
+++ src/프로그래머스2단계100점못맞은문제들/연속된부분수열의합2.java
@@ -1,7 +1,66 @@
 package 프로그래머스2단계100점못맞은문제들;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class 연속된부분수열의합2 {
     public static void main(String[] args) {
+        int[] sequence = {1, 1, 1, 2, 3, 4, 5};
+        int k = 5;
+        System.out.println(solution(sequence,k));
+    }
 
+    private static int[] solution(int[] sequence, int k) {
+
+        int left = 0;
+        int right = 0;
+
+        int start = 0;
+
+        int end = sequence.length;
+
+        HashMap<String,String> map = new HashMap<>();
+        while (right<sequence.length){
+            int sum = 0;
+            String idx = "";
+            String len = "";
+            for (int i = left; i <= right; i++) {
+                sum+=sequence[i];
+                idx+=String.valueOf(i);
+                len += String.valueOf(sequence[i]);
+            }
+
+            if(sum == k){
+                map.put(len,idx);
+            }
+
+            if(sum<=k){
+                right++;
+            }
+            else if(sum>k) {
+                left++;
+            }
+        }
+
+        int min = sequence.length;
+
+        String ans = "";
+        for (String s : map.keySet()) {
+            if(min > s.length()){
+                min = s.length();
+                ans = map.get(s);
+            }
+        }
+
+        int[] answer = new int[2];
+        if(ans.length() == 1){
+            answer[0] = Integer.parseInt(String.valueOf(ans.charAt(0)));
+            answer[1] = Integer.parseInt(String.valueOf(ans.charAt(0)));
+        }
+        else {
+            answer[0] = Integer.parseInt(String.valueOf(ans.charAt(0)));
+            (answer[1] = Integer.parseInt(String.valueOf(ans.charAt(1)));
+        }
+        return answer;
     }
 }
