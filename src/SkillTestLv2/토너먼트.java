c615c5bbf770551c27788669fcbf83a99eea9c68
zack8361 <zack6353@naver.com>
2023-08-01 21:03:23 +0900

    토너
    토너

--- src/SkillTestLv2/토너먼트.java
+++ src/SkillTestLv2/토너먼트.java
@@ -11,10 +11,16 @@ public class 토너먼트 {
     private static int solution(int n, int a, int b) {
         int ans =0;
         System.out.println();
-        while (a!=b){
-            a = (a/2) +(a%2);
-            b = (b/2) + (b%2);
-            ans++;
+        ans ++;
+        if(ans>1){
+            return -1;
+        }
+        else {
+            while (a != b) {
+                a = (a / 2) + (a % 2);
+                b = (b / 2) + (b % 2);
+                ans++;
+            }
         }
         return ans;
     }
