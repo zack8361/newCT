3b61af7e7019392dfadb00bd49eee60003f9723a
zack8361 <zack6353@naver.com>
2023-08-03 23:39:35 +0900

    Update 자율주제.java

--- src/SkillTestLv2/자율주제.java
+++ src/SkillTestLv2/자율주제.java
@@ -4,5 +4,8 @@ public class 자율주제 {
     public static void main(String[] args) {
         int n = 2;
         System.out.println(n);
+        for (int i = 0; i < n; i++) {
+            System.out.println("i = " + i);
+        }
     }
 }
